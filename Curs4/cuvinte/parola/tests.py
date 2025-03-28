d9b56a744b5f647bf668b5ea859b9cdce491a467
Dan <dan.borzea@live.com>
2025-03-28 21:02:07 +0200

    parole.csv

--- Curs4/cuvinte/parola/tests.py
+++ Curs4/cuvinte/parola/tests.py
@@ -3,15 +3,21 @@ from django.test import TestCase
 # Create your tests here.
 
 
-print(ord('A'))
-print(ord('B'))
-print(ord('E'))
+# print(ord('A'))
+# print(ord('B'))
+# print(ord('E'))
 
-print(ord('Z'))
+# print(ord('Z'))
 
-print(ord('a'))
+# print(ord('a'))
 
-print(chr(65))
+# print(chr(65))
 
-for i in range(ord('A'), ord('Z')+1):
-    print ()
+# for i in range(ord('A'), ord('Z')+1):
+#     print ()
+import pandas as pd
+
+with open("parole.csv", "w") as fwriter:
+    fwriter.write_through("parole")
+
+df = pd.read_csv("parole.csv")
--- Curs4/cuvinte/parola/views.py
+++ Curs4/cuvinte/parola/views.py
@@ -5,12 +5,14 @@ from django.http import HttpResponse
 import random
 import string
 
+import pandas as pd
+
 
 CIFRE = [str(i) for i in range(10)]
 LITERE_MARI = [chr(i) for i in range(ord('A'), ord('Z')+1)]
 LITERE_MICI = [chr(i) for i in range(ord('a'), ord('a')+1)]
 
-OPTIONS = [string.ascii_lowercase, string.ascii_uppercase, string.digits, string.punctuation]
+
 
 def random_view(request):
     OPTIUNI = CIFRE + LITERE_MICI + LITERE_MARI
@@ -27,11 +29,31 @@ def alege_view(request):
 
     }
     lungime = request.GET.get('lungime')
-    if lungime:
+
+    OPTIONS = string.ascii_lowercase 
+    if request.GET.get('withupper'):
+        OPTIONS += string.ascii_uppercase
+    if request.GET.get('withdigits'):
+        OPTIONS += string.digits
+    if request.GET.get('withspecial'):
+        OPTIONS += string.punctuation
+
+
+#+ string.ascii_uppercase + string.digits + string.punctuation
+    print(lungime)
+    try:
+        lungime=int(lungime)
         parola = ""
-        for i in range(100):
+        for i in range(lungime):
             parola += random.choice(OPTIONS)
 
-        context['parola'] = '1234'
+        context['parola'] = parola
+
+        df = pd.read_csv('parole.csv', index_col=0)
+        df.loc[len(df)] = parola
+        df.to_csv("parole.csv")
+        print("df=", df)
+    except:
+        pass
 
     return render(request, "alege.html", context)
