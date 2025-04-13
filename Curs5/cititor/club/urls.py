aaf100d40e6bebab6479c72394cc16b8f8d03f2b
Dan <dan.borzea@live.com>
2025-04-13 21:57:31 +0300

    pauza

--- Curs5/cititor/club/urls.py
+++ Curs5/cititor/club/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import validare_cnp_view
+from .views import validare_cnp_view, rezultat_cnp_view
 #  url mic
 
 urlpatterns = [
-    path('cnp', validare_cnp_view)
+    path('cnp', validare_cnp_view),
+    path('rezultat', rezultat_cnp_view)
 ]
